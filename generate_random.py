163191eea16b8fbc1b376813d6dff0798faa9758
caiomcg <caiomcg@gmail.com>
2017-09-13 12:24:38 -0300

    Reajust to counting sort

--- generate_random.py
+++ generate_random.py
@@ -9,4 +9,4 @@ if __name__ == "__main__": # Check if executed, not imported
     if len(sys.argv) > 2: # Verify if the amount of arguments is correct
         with open (sys.argv[1], 'a') as f:  # Open the output file
             for element in range(0, int(sys.argv[2])): # Iterate through a range
-                f.write(str(randint(0, 1000)) + "\n") # Write the randomized number
+                f.write(str(randint(1, 1000)) + "\n") # Write the randomized number
